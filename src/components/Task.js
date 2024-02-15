2c65435b5fa4fca8aaf99244b82f32c4861777d9
Kairi Wright <kanwright19@gmail.com>
2024-02-15 02:20:52 -0500

    completed

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Task() {
+
+
+function Task({text, category, removeTask, id}) {
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={() => removeTask(id)}>X</button>
     </div>
   );
 }
